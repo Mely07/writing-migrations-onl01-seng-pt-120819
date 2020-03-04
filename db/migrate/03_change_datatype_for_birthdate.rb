7883aa32de227becc872364a9819269374dcb20e
Damely Tineo <damely.tineo07@gmail.com>
2020-03-04 03:11:15 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,7 +1,6 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
-def change
-  gration will have the same setup as the last. Be sure to use the change_column method. It takes three necessary arguments: 
+  def change
+    change_column(table_name, column_name, type)
   end
-end
 
 end
